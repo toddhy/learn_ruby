206d47fe9b2f867516644a3ecf8935af1905be5b
Todd <toddhy@hawaii.edu>
2018-07-23 19:33:45 +0000

    Completed 00, 01, and half of 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
-#write your code here
+def ftoc far
+	((far - 32) * (5.0 / 9.0)).floor
+end
+
+def ctof cel
+	(cel * (9.0/5.0) + 32)
+end
+
+# F is 5/9 of 1 degree C
+# # 0 degree C is 32 F
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
-#write your code here
+def add num1, num2
+	num1 + num2
+end
+
+def subtract num1, num2
+	num1 - num2
+end
+
+def sum array
+	count = array.length
+	sum = 0
+	
+	while count > 0
+		sum += array[count - 1]
+		count -= 1
+	end
+	
+	sum
+
+end
